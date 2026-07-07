271a1293b6c10e769541320fdeadd0004e211561
avinashtech-code <avinash.tech149@gmail.com>
2026-07-07 22:49:10 +0530

    Update Part5.py

--- Part5.py
+++ Part5.py
@@ -22,12 +22,14 @@ def view() :
     return data 
 
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id: str = Path(..., description = "id of patient",example ="P001")):
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
     data = load_data()
+
     if patient_id in data:
         return data[patient_id]
-    else :
-        return {"error" : "patient not found"}
+    raise HTTPException(status_code=404, detail='Patient not found')
+
     
     
 @app.get('/sort')
@@ -48,3 +50,5 @@ def sort_patients(sort_by: str = Query(..., description='Sort on the basis of he
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
 
     return sorted_data
+
+
