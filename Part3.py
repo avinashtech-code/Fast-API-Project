c8c00366be2c6ab52e74ea218a8f97c3c4b06153
avinashtech-code <avinash.tech149@gmail.com>
2026-07-07 16:11:54 +0530

    Create Part3.py

---
+++ Part3.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import json 
+
+app = FastAPI() 
+
+
+def load_data():
+    with open('patient.json' , 'r') as f :
+        data = json.load(f) 
+    return data 
+@app.get("/")
+def hello() :
+    return {'message':'Patient Management System'}
+
+@app.get("/about")
+def about():
+    return {"message": "This a patient related project"}
+
+@app.get("/view")
+def view() :
+    data = load_data() 
+    return data 
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    else :
+        return {"error" : "patient not found"}
+    
+    
+
+
